5da98ced3088728b87360776d9fdc834b34da625
Carlos <carloscl1986@gmail.com>
2019-01-17 12:39:28 +0100

    n version

---
+++ staff/carlos-calvo/array-high-order-functions/splice.js
@@ -0,0 +1,15 @@
+function splice(array, start, end, element){
+    if(!array instanceof Array) throw new Error('Not an array')
+    if(isNaN(start)) throw new Error('Not a number 1st')
+    if(isNaN(end)) throw new Error('Not a number 2nd')
+    if(arguments.length ==3){ //delete
+        
+    }
+
+    if(arguments.length > 3){
+        
+        for(var i = 4; i)
+    }
+
+
+}
