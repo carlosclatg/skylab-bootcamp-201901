78556f4e3eb6abdfaff75d2526b46ec16b51ef97
Ury18 <ury_98@hotmail.com>
2019-02-04 09:59:38 +0100

    detailedPokemon set ready to recive data

--- staff/groups/funciton/funciton-pokedex/src/App.js
+++ staff/groups/funciton/funciton-pokedex/src/App.js
@@ -4,8 +4,157 @@ import './App.css';
 import './components/DetailedPokemonPanel'
 import DetailedPokemonPanel from './components/DetailedPokemonPanel';
 
+
 class App extends Component {
+  state={
+    
+     statsReal : [
+    
+      {
+        base_stat: 48,
+    
+        stat: {
+    
+          name:'speed'
+    
+    
+        }
+    
+    
+      },
+      {
+        base_stat: 48,
+    
+        stat: {
+    
+          name:'special-defense'
+    
+    
+        }
+    
+      },
+      {
+        base_stat: 48,
+    
+        stat: {
+    
+          name:'special-attack'
+    
+    
+        }
+    
+      },
+      {
+        base_stat: 48,
+    
+        stat: {
+    
+          name:'defense'
+    
+    
+        }
+    
+      },
+      {
+        base_stat: 48,
+    
+        stat: {
+    
+          name:'attack'
+    
+    
+        }
+    
+      },
+      {
+        base_stat: 48,
+    
+        stat: {
+    
+          name:'hp'
+    
+    
+        }
+    
+      }
+    
+    
+    
+    ],
+    
+    realAbilities :
+    [
+    
+      {
+    
+        ability:{
+          name:'imposter'
+        }
+    
+      },
+      {
+    
+        ability:{
+          name:'limber'
+        }
+    
+      }
+    
+    
+    
+    
+    ],
+    
+    RealMoves : [
+    
+    {
+      move:{
+    
+        name:'transform'
+    
+    
+      }
+    }
+    
+    
+    
+    ],
+    
+    
+    RealTypes:[
+    
+    {
+      type:{
+        name:'normal'
+      }
+    }
+    
+    ],
+    
+    RealHeldItems :[
+    
+    {
+      item:{
+        name:'metal-powder'
+      }
+    },
+    {
+      item:{
+        name:'quick-powder'
+      }
+    }
+    
+    
+    ]
+
+
+
+}
+
+
   render() {
+    const { state: { statsReal, realAbilities, RealMoves, RealTypes,RealHeldItems }} = this
+
     return (
       <div className="App">
         <header className="App-header">
@@ -23,11 +172,11 @@ class App extends Component {
           </a>
         </header>
 
-          <DetailedPokemonPanel></DetailedPokemonPanel>
+        <DetailedPokemonPanel name={'ditto'} stats={statsReal} abilities={realAbilities} moves={RealMoves} types = {RealTypes} heldItems = {RealHeldItems} heigth={'3'}weight={'3'}/>
 
 
 
-      </div>
+      </div >
     );
   }
 }
--- staff/groups/funciton/funciton-pokedex/src/components/DetailedPokemonPanel/index.js
+++ staff/groups/funciton/funciton-pokedex/src/components/DetailedPokemonPanel/index.js
@@ -4,118 +4,57 @@ import './index.sass'
 
 class DetailedPokemonPanel extends Component {
 
-    state = {
-
-        id: null,
-        name: null,
-        abilities: [],
-        heigth: null,
-        heldItems: [],
-        moves: [],
-        sprites: [],
-        stats: [],
-        weight: null,
-        types: []
-
-    }
-
-
     render() {
+       
+        const { props: { name, stats, abilities, moves, types,heldItems,heigth,weight }} = this
+
         return <section>
 
-            <h2>Ditto</h2>
+            <h2>${name}</h2>
             <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'></img>
-            <p>Height: <span>3</span></p>
-            <p>weight: <span>40</span></p>
+            <p>Height: <span>{heigth}</span></p>
+            <p>weight: <span>{weight}</span></p>
             <ul>
                 <h3>stats</h3>
-                <li>Speed: <span> 48</span></li>
-                <li>Special Defense: <span> 48</span></li>
-                <li>Special Attack: <span> 48</span></li>
-                <li>Defense: <span> 48</span></li>
-                <li>Attack: <span> 48</span></li>
-                <li>HP: <span> 48</span></li>
+                <li>Speed: <span> {stats[0].base_stat}</span></li>
+                <li>Special Defense: <span> {stats[1].base_stat}</span></li>
+                <li>Special Attack: <span> {stats[2].base_stat}</span></li>
+                <li>Defense: <span> {stats[3].base_stat}</span></li>
+                <li>Attack: <span> {stats[4].base_stat}</span></li>
+                <li>HP: <span> {stats[5].base_stat}</span></li>
 
             </ul>
 
             <ul>
                 <h3>Abilities</h3>
 
-                <li>Imposter</li>
-                <li>Limber</li>
+                <li>{abilities[0].ability.name}</li>
+                <li>{abilities[1].ability.name}</li>
 
 
             </ul>
 
             <ul>
                 <h3>Moves</h3>
-                <li>Transform</li>
+                <li>{moves[0].move.name}</li>
 
 
             </ul>
 
             <ul>
                 <h3>Types</h3>
-                <li>Normal</li>
+                <li>{types[0].type.name}</li>
 
             </ul>
 
             <ul>
                 <h3>Held Items</h3>
-                <li>Metal Powder</li>
-                <li>Quick Powder</li>
+                <li>{heldItems[0].item.name}</li>
+                <li>{heldItems[1].item.name}</li>
 
             </ul>
 
         </section>
-
-        // return <section>
-
-        //     <h2>${name}</h2>
-        //     <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'></img>
-        //     <p>Height: <span>${heigth}</span></p>
-        //     <p>weight: <span>${weight}</span></p>
-        //     <ul>
-        //         <h3>stats</h3>
-        //         <li>Speed: <span> ${stats[0].base_stat}</span></li>
-        //         <li>Special Defense: <span> ${stats[1].base_stat}</span></li>
-        //         <li>Special Attack: <span> ${stats[2].base_stat}</span></li>
-        //         <li>Defense: <span> ${stats[3].base_stat}</span></li>
-        //         <li>Attack: <span> ${stats[4].base_stat}</span></li>
-        //         <li>HP: <span> ${stats[5].base_stat}</span></li>
-
-        //     </ul>
-
-        //     <ul>
-        //         <h3>Abilities</h3>
-
-        //         <li>${abilities[0].ability.name}</li>
-        //         <li>${abilities[1].ability.name}</li>
-
-
-        //     </ul>
-
-        //     <ul>
-        //         <h3>Moves</h3>
-        //         <li>${moves[0].move.name}</li>
-
-
-        //     </ul>
-
-        //     <ul>
-        //         <h3>Types</h3>
-        //         <li>${types[0].type.name}</li>
-
-        //     </ul>
-
-        //     <ul>
-        //         <h3>Held Items</h3>
-        //         <li>{heldItems[0].item.name}</li>
-        //         <li>{heldItems[1].item.name}</li>
-
-        //     </ul>
-
-        // </section>
 
 
     }
