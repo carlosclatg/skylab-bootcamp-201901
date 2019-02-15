c2b9c1adc8e2683cade533deabaddacbeee476be
Carlos <carloscl1986@gmail.com>
2019-02-15 19:58:13 +0100

    version viernes tarde

---
+++ staff/carlos-calvo/web-app/index.js
@@ -0,0 +1,181 @@
+require('dotenv').config()
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+// const FileStore = require('session-file-store')(session)
+const logicFactory = require('./src/logic-factory')
+const artistList = null 
+const albumList =  null
+const trackList = null
+
+const { env: { PORT }, argv: [, , port = PORT || 8080] } = process
+
+const app = express()
+
+app.use(session({
+    secret: 'a secret phrase',
+    resave: true,
+    saveUninitialized: true,
+    // store: new FileStore({
+    //     path: './.sessions'
+    // })
+}))
+
+app.use(express.static('public'))
+
+app.set('view engine', 'pug')
+app.set('views', './src/components')
+
+const formBodyParser = bodyParser.urlencoded({ extended: false })
+
+function pullFeedback(req) {
+    const { session: { feedback } } = req
+
+    req.session.feedback = null
+
+    return feedback
+}
+
+
+
+app.get('/', (req, res) => {
+    res.render('landing')
+})
+
+app.get('/register', (req, res) => {
+    const logic = logicFactory.create(req)
+
+    if (logic.isUserLoggedIn) {
+        res.redirect('/home')
+    } else {
+        const feedback = pullFeedback(req)
+
+        res.render('register', { feedback })
+    }
+})
+
+app.post('/register', formBodyParser, (req, res) => {
+    const { body: { name, surname, email, password, passwordConfirm } } = req
+
+    const logic = logicFactory.create(req)
+
+    try {
+        logic.registerUser(name, surname, email, password, passwordConfirm)
+            .then(() => res.render('register-success', {name, email}))
+            .catch(({ message }) => {
+                req.session.feedback = message
+
+                res.redirect('/register')
+            })
+    } catch ({ message }) {
+        req.session.feedback = message
+
+        res.redirect('/register')
+    }
+})
+
+app.get('/login', (req, res) => {
+    const logic = logicFactory.create(req)
+
+    if (logic.isUserLoggedIn) {
+        res.redirect('/home')
+    } else {
+        const feedback = pullFeedback(req)
+
+        res.render('login', { feedback })
+    }
+})
+
+app.post('/login', formBodyParser, (req, res) => {
+    const { body: { email, password } } = req
+
+    const logic = logicFactory.create(req)
+
+    try {
+        logic.logInUser(email, password)
+            .then(() => res.redirect('/home'))
+            .catch(({ message }) => {
+                req.session.feedback = message
+
+                res.redirect('/login')
+            })
+    } catch ({ message }) {
+        req.session.feedback = message
+
+        res.redirect('/login')
+    }
+})
+
+app.get('/home', (req, res) => {
+    try {
+        const { session: { feedback } } = req
+
+        const logic = logicFactory.create(req)
+
+        if (logic.isUserLoggedIn)
+            logic.retrieveUser()
+                .then(user => res.render('home',{name : user.name, feedback} ))
+                .catch(({ message }) => {
+                    req.session.feedback = message
+
+                    res.redirect('/home', artistList)
+                })
+        else res.redirect('/login')
+    } catch ({ message }) {
+        req.session.feedback = message
+
+        res.redirect('/home')
+    }
+})
+
+app.post('/logout', (req, res) => {
+    const logic = logicFactory.create(req)
+    logic.logOutUser()
+
+    res.redirect('/searchArtist')
+})
+
+
+app.post('/searchArtist', formBodyParser, (req, res) => {
+    const { body: { artist } } = req
+    res.redirect(`/searchArtist/${artist}`)  
+})
+
+
+app.get('/searchArtist/:artist', (req, res) =>{
+    const { params: { artist } } = req
+    
+    try{
+        const logic = logicFactory.create(req)
+        logic.searchArtists(artist)
+            .then(results =>{
+                artistList = results
+                res.render('artist', {results})
+                }) 
+    } catch (error){
+    }
+})
+
+///app.get('*', (req, res) => res.status(404).render('Componente404'))
+
+app.get('*', (req, res) => res.send(404, renderPage(`<section class="not-found">
+        <h2>NOT FOUND</h2>
+
+        Go <a href="/">Home</a>
+    </section>`)))
+
+function renderPage(content) {
+    return `<html>
+<head>
+    <title>HELLO WORLD!</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="main">
+    <h1>HELLO WORLD! 🤡</h1>
+    ${content}
+</body>
+</html>`
+}
+
+app.listen(port, () => console.log(`server running on port ${port}`))
